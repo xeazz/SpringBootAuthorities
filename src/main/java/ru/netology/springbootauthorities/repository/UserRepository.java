cefce7d442e5aff4180e5586ef4488addb9bf1c1
Dima Vorobev <dimjkee@icloud.com>
2023-02-25 10:59:33 +0300

    correct UserRepository.java

--- src/main/java/ru/netology/springbootauthorities/repository/UserRepository.java
+++ src/main/java/ru/netology/springbootauthorities/repository/UserRepository.java
@@ -4,39 +4,29 @@ import org.springframework.stereotype.Repository;
 import ru.netology.springbootauthorities.authorities.Authorities;
 import ru.netology.springbootauthorities.model.Person;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 @Repository
 public class UserRepository {
-    Map<String, Person> personMap = new HashMap<>();
+    Map<Person, List<Authorities>> personMap = new HashMap<>();
 
     {
-        personMap.put("Dima", new Person("Dima", "1234"));
-        personMap.put("Tanya", new Person("Tanya", "qwe123"));
-        personMap.put("Pavel", new Person("Pavel", "zXc!"));
-        personMap.put("Vyacheslav", new Person("Vyacheslav", "234cd1"));
+        personMap.put(new Person("Dima", "1234"), List.of(Authorities.values()));
+        personMap.put(new Person("Tanya", "qwe123"), List.of(Authorities.READ));
+        personMap.put(new Person("Pavel", "zXc!"), List.of(Authorities.WRITE));
+        personMap.put(new Person("Vyacheslav", "234cd1"), List.of(Authorities.WRITE, Authorities.DELETE));
     }
 
     public List<Authorities> getUserAuthorities(Person person) {
-        List<Authorities> authoritiesList = new ArrayList<>();
-        Person newPerson = personMap.get(person.getUser());
-        if (personMap.containsKey(person.getUser()) && newPerson.getPassword().equals(person.getPassword())) {
-            if ("Dima".equals(person.getUser())) {
-                authoritiesList.add(Authorities.READ);
-                authoritiesList.add(Authorities.DELETE);
-                authoritiesList.add(Authorities.WRITE);
-            } else if ("Tanya".equals(person.getUser())) {
-                authoritiesList.add(Authorities.READ);
-            } else if ("Pavel".equals(person.getUser())) {
-                authoritiesList.add(Authorities.WRITE);
-            } else if ("Vyacheslav".equals(person.getUser())) {
-                authoritiesList.add(Authorities.WRITE);
-                authoritiesList.add(Authorities.DELETE);
+        for (Person newPerson : personMap.keySet()) {
+            if (newPerson.getUser().equals(person.getUser()) &&
+                    newPerson.getPassword().equals(person.getPassword())) {
+                return personMap.get(newPerson);
             }
         }
-        return authoritiesList;
+        return null;
     }
+
 }
